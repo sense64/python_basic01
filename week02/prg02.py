913f673b841e9b4cdd0c5ab758d49f212816afdb
sense64 <62490803+sense64@users.noreply.github.com>
2020-09-01 14:27:24 +0900

    week02 Example Python Programs

--- week02/prg02.py
+++ week02/prg02.py
@@ -1,3 +1,6 @@
+# https://github.com/sense64/python_basic01
+# github에 실습파일이 있습니다.
+
 a= 1000
 b =78.95
 #서식문자가 없는 경우
@@ -6,7 +9,7 @@ print(a, "   ", b)
 #서식문자가 있는 경우 
 print("%d" % a)
 print("%5d" % a)
-print("%05d" % a)
+print("%05d" % a, end="  ")
 
 print("%f" % b)
 print("%7.1f" % b)
@@ -16,6 +19,6 @@ print("%s" % "동아대학교")
 print("%10s" % "경영정보학과")
 
 #format() 함수 사용
-
+print(format(a, "x"))
 print("{1}, {0}" .format(a, b))
 print("{1:5.1f} {0:5d}" .format(a, b))
---
+++ week02/prg03.py
@@ -0,0 +1,12 @@
+#turtle graphics programing
+import turtle  #모듈을 가져옴
+
+turtle.shape('turtle')
+turtle.color('red')
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
